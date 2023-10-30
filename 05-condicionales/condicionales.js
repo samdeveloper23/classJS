c2ec5787fb4fe481cb0f057c16d7b5eb86486ea3
samdeveloper23 <sam.developer.23@gmail.com>
2023-10-30 13:31:11 +0100

    segunda subida

--- 05-condicionales/condicionales.js
+++ 05-condicionales/condicionales.js
@@ -30,3 +30,48 @@ if (edad1 >= edad2) {
 } else{
     console.log('edad1 es nemor que edad2');
 }
+
+/**
+ * ############ OPERADORES LÓGICOS  ##############
+ * 
+ * && = AND (Y)
+ * || = OR (O)
+ * !  = negación
+ * 
+ */
+const year = 2023;
+//  negación
+if (year != 2013) {
+    console.log('No! Estámos en el 2023.');
+}
+// OR
+if (year == 2012 || year == 2023) {
+    console.log('Cómo no es 2012 pasa a la siguente condición, la cuál si se cumpre a ser 2023.');
+}
+//  AND
+if (year != 2012 && year == 2023) {
+    console.log('La condición se cumpre en ambos casos por lo que se ejecuta esta linea.');
+}
+
+// También podremos enlazár
+const six = 6
+if (six == 8 || (six >= 3 && six <=9)) {
+    console.log('Se cumpre la segunda condición');
+}else{
+    console.log('Si cambiámos el valor de "six" a "2", entonces no se cumpriría.');
+}
+
+//  ############### SWING #################
+const edad = 18;
+const imprime = '';
+
+switch (edad) {
+    case 18:
+        imprime = 'Tiene 18 años';
+        break;
+    case 19:
+        imprime = 'Tiene 19 años';
+        break;
+    default:
+        break;
+}
